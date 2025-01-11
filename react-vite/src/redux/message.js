bf0357661a87efe145a055fb7dae51a5f3f8d6c6
glitterpillz <karenhickey1010@gmail.com>
2025-01-11 14:01:32 -0500

    added MessageContext provider to context - UserInboxPage conditionally rendering sent and received messages

--- react-vite/src/redux/message.js
+++ react-vite/src/redux/message.js
@@ -2,13 +2,14 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
     inbox: [],
+    sentBox: [],
     messageDetails: null,
     loading: false,
     errors: null
 }
 
 export const getUserInbox = createAsyncThunk(
-    "/message/userInbox",
+    "/message/getUserInbox",
     async (_, { rejectWithValue }) => {
         try {
             const response = await fetch('/api/messages/inbox', {
@@ -24,6 +25,24 @@ export const getUserInbox = createAsyncThunk(
         }
     }
 )
+
+export const getUserSentBox = createAsyncThunk(
+    "/message/getUserSentBox",
+    async (_, { rejectWithValue }) => {
+        try {
+            const response = await fetch('/api/messages/sent', {
+                method: 'GET'
+            });
+            const data = await response.json();
+            if (!response.ok) {
+                throw new Error(`Error getting user sent box: ${data.message}`);
+            }
+            return data;
+        } catch (error) {
+            return rejectWithValue(error.message || "Error fetching sent box");
+        }
+    }
+)   
 
 const MessageSlice = createSlice({
     name: "message",
@@ -42,6 +61,18 @@ const MessageSlice = createSlice({
             .addCase(getUserInbox.fulfilled, (state, action) => {
                 state.loading = false;
                 state.inbox = action.payload;
+            })
+            .addCase(getUserSentBox.pending, (state) => {
+                state.loading = true;
+                state.errors = null;
+            })
+            .addCase(getUserSentBox.rejected, (state, action) => {
+                state.loading = false;
+                state.errors = action.payload;
+            })
+            .addCase(getUserSentBox.fulfilled, (state, action) => {
+                state.loading = false;
+                state.sentBox = action.payload;
             })
     }
 })
