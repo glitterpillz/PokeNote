3404e1676653832b571cf24083010bd47fbd257e
glitterpillz <karenhickey1010@gmail.com>
2025-01-20 18:13:39 -0500

    changes to profile page - package json

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -6,19 +6,11 @@ from .user_pokemon import seed_user_pokemon, undo_user_pokemon
 
 from app.models.db import db, environment, SCHEMA
 
-# Creates a seed group to hold our commands
-# So we can type `flask seed --help`
 seed_commands = AppGroup('seed')
 
-
-# Creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        # Before seeding in production, you want to run the seed undo 
-        # command, which will  truncate all tables prefixed with 
-        # the schema name (see comment in users.py undo_users function).
-        # Make sure to add all your other model's undo functions below
         undo_users()
         undo_pokemon()
         undo_journal_entries()
@@ -27,14 +19,11 @@ def seed():
     seed_pokemon()
     seed_journal_entries()
     seed_user_pokemon()
-    # Add other seed functions here
 
 
-# Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
     undo_users()
     undo_pokemon()
     undo_journal_entries()
     undo_user_pokemon()
-    # Add other undo functions here
